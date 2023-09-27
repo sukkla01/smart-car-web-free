b29acd630bc43dd0df0100f49747666aae54aeaf
sukkla01 <sukkla01@gmail.com>
2023-09-27 16:33:57 +0700

    config

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const config = {
     font: 'Kanit',
-    BASE_URL: 'https://smart-car-api.diligentsoftinter.com',
     // BASE_URL: 'http://localhost:4090',
-    // BASE_URL: 'http://192.168.0.29:4090',//
+    BASE_URL: 'http://192.168.0.xx:4090',//
 }
 
 
